1dc17b0efca6ac373e07be3268341c97de00e1da
Matthew Lonis <mattlonis@gmail.com>
2024-12-01 03:34:20 +0000

    Upgrade @angular/core from 18.2.12 to 19.0.1

--- projects/ngx-example-theme/src/lib/ngx-example-theme.component.ts
+++ projects/ngx-example-theme/src/lib/ngx-example-theme.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'lib-ngx-example-theme',
-  standalone: true,
   imports: [],
   template: ` <p>ngx-example-theme works!</p> `,
   styles: ``,
