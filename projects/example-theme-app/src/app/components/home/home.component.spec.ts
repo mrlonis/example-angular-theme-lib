7caa98659996d4eb206166136b398a6a19b84199
Matthew Lonis <mattlonis@gmail.com>
2026-07-07 20:43:42 -0400

    Upgrade to Angular 20

--- projects/example-theme-app/src/app/components/home/home.component.spec.ts
+++ projects/example-theme-app/src/app/components/home/home.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { provideNoopAnimations } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
@@ -9,7 +8,6 @@ describe('HomeComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HomeComponent],
-      providers: [provideNoopAnimations()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
